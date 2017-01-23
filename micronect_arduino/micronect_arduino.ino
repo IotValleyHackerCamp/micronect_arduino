5e7942358e6fc0197704427d907b9837882a30aa
cometurrata <come.on.ct@gmail.com>
2017-01-23 21:04:32 +0100

    first commit, getting analog read from photoresistor working

---
+++ micronect_arduino/micronect_arduino.ino
@@ -0,0 +1,23 @@
+int sensorPin = A5;    // select the input pin for the potentiometer
+int ledPin = 13;      // select the pin for the LED
+int sensorValue = 0;  // variable to store the value coming from the sensor
+
+void setup() {
+
+   Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+}
+
+void loop() {
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);
+
+  // print that sensor value over serial
+  Serial.print(sensorValue);
+  Serial.print("\n");
+  delay(30);
+
+  
+}
