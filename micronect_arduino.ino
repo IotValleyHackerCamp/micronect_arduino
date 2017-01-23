5bb9c9729d5e00e077e2c8ffd7a988719ac5d194
cometu <come.on.ct@gmail.com>
2017-01-23 22:33:09 +0100

    Update micronect_arduino.ino

--- micronect_arduino.ino
+++ micronect_arduino.ino
@@ -1,13 +1,13 @@
-int sensorPin = A5;    // select the input pin for the potentiometer
-int sensorValue = 0;  // variable to store the value coming from the sensor
+int sensorPin = A5;    // select the input pin photoresistor (LDR)
+int sensorValue = 0;  // variable to store the value coming from the input Pin
 
 void setup() {
-   Serial.begin(9600);
+   Serial.begin(9600); // chossing randomly 9600 baud as communication speed
    while (!Serial) {}; // wait for serial port to connect. Needed for native USB port only
 }
 
 void loop() {
-  // read the value from the sensor:
+  // read the value from the sensor Pin:
   sensorValue = analogRead(sensorPin);
 
   // print that sensor value over serial
